17aae560f06fa31151461311bf182eb2633135ff
johnericward <johnericward@gmail.com>
2019-07-10 22:26:54 -0400

    added a background and some test alerts to the app.js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+var score = 0;
+alert("Welcome to the QUIZ!");
+var q1;
+
+q1 = prompt("what is the captial of the United States?")
+if (q1 == "Washington DC") {
+    score++;
+    console.log("Correct!");
+} else {
+    console.log("Wrong! :(");
+}
+
+var q2;
+
+q2 = prompt("what is the capital of the United States?")
+if (q2 == "Helsinki") {
+    score++;
+    console.log("Correct!");
+} else {
+    console.log("Wrong! :(");
+}
